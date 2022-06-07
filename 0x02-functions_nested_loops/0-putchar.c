082b2b42290d6b06b99784668967379f0a248cc2
Nomoya Magazi <nomoya.magazi@gmail.com>
2022-06-07 10:54:02 -0700

    putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h
+/**
+ * main-entry point
+ * Return: is 0
+ */
+int main(void)
+{
+	charXo[] = "_putchar";
+	int i;
+
+	for (i=0;i<8;i++)
+	{
+		_putchar(Xo[i]);
+	}
+	_putchar('\n');
+	return (0)
+}
