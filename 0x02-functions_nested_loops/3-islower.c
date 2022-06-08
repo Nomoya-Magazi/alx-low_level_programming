7739c1fe0bd23f9d6ba95c7e10587420fc66442c
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-06-08 11:26:54 +0200

    Update 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - is the entry
+ * @c: character in ASCII code
+ * Return: 1 is lowercase and 0 if uppercase
+ */
+int _islower(int c)
+{
+	char i;
+	int low = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			low = 1;
+	}
+	return (low);
+}
