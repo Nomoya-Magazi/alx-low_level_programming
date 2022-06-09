8f40e77c731932a52af37c601e22a5d2f40d1c30
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-06-09 19:22:35 +0200

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-include "main.h"
+include ";main.h"
 void positive_or_negative(int i);
 /**
  *main - tests function that prints if integer is positive or negative
@@ -10,7 +10,7 @@ int main(void)
 int i;
 
 i = 0;
-positive_or_negative(i);
+'positive_or_negative'(i);
 
 return (0);
 }
