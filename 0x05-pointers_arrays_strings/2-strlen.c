e8d56fa5563705287708b0e24bccd329f6f7478c
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-06-13 20:27:52 +0200

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,14 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _strlen - returns the length of a seting
+ * @s: is the input
+ * Return: nothing
  */
-int main(void)
+int _strlen(char *s)
 {
-    char *str;
-    int len;
+	int len = 0;
 
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
-    return (0);
+	while (s[len] != '\0')
+		len++;
+	return (len);
 }
