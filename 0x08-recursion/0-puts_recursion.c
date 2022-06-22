73c515e415476831dd46c8bd1c09ff9387c35a67
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-06-22 20:30:09 +0200

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -14,26 +14,6 @@ void _puts_recursion(char *s)
 		_puts_recursion(s + 1);
 	}
 
-	else 
-		_putchar('\n');
-}
-=======
-#include "main.h"
-/**
- * _puts_recursion - prints a string, followed by a new line
- * @s: the input
- * Return: nothing
- */
-
-void _puts_recursion(char *s)
-{
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-
 	else
 		_putchar('\n');
 }
->>>>>>> b98846c3e857d0ad5f4dc84258ed518927192707
