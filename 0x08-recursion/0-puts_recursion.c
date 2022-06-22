384ec85cf2cf70030478b0aecdc4e90c8afa513a
Nomoya Magazi <nomoya.magazi@gmail.com>
2022-06-22 11:25:38 -0700

    update task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,29 +1,10 @@
-<<<<<<< HEAD
-#include "main.h"
-/**
- * _puts_recursion -  prints a string, followed by a new line
- * @s: the input
- * Return: nothing
- */
-
-void _puts_recursion(char *s);
-{
-	int i = 0;
-	while (s[i] ! = '\0')
-	{
-		_putchar9s[i];
-		i +=;
-	}
-	-putchar('\n';)
-}
-
-=======
 #include "main.h"
 /**
  * _puts_recursion - prints a string, followed by a new line
  * @s: the input string
  * Return: nothing
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s)
@@ -32,7 +13,6 @@ void _puts_recursion(char *s)
 		_puts_recursion(s + 1);
 	}
 
-	else
+	else 
 		_putchar('\n');
 }
->>>>>>> 7444dbe672092221f131eb4a1f7fd3e6003d2db7
