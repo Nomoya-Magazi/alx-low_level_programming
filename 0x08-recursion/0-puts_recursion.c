9e39a9c3a2a1eb1be4f821293e347a28fadcf95a
Nomoya Magazi <nomoya.magazi@gmail.com>
2022-06-22 10:57:03 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion -  prints a string, followed by a new line
+ * @s: the input
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s);
+{
+	int i = 0;
+	while (s[i] ! = '\0')
+	{
+		_putchar9s[i];
+		i +=;
+	}
+	-putchar('\n';)
+}
+
