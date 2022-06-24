0cc9c19a1886c88e8ce78b8da2fb99b44864e716
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-06-24 21:38:56 +0200

    Update _putchar.c

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-
-
-=======
->>>>>>> f487ec7ef693b97cdb0ea2414eade22f8a797357
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
@@ -15,7 +10,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> f487ec7ef693b97cdb0ea2414eade22f8a797357
