caec58a60854ed9920311907af052211c046464f
Nomoya Magazi <nomoya.magazi@gmail.com>
2022-06-24 12:18:40 -0700

    static library

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-=======
+
+
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
@@ -12,4 +12,4 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
->>>>>>> 01c68ac6a536b6f3dfd975dd470e2a7c40447200
+
