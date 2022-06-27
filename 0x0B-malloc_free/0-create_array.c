88eda4d17da52d8ac414b6f7468afd13411c01ee
Nomoya Magazi <nomoya.magazi@gmail.com>
2022-06-27 14:16:37 -0700

    malloc and free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars,
+ * and initializes it with a specific char
+ * @c: is the input
+ * @size: the size of the array
+ * Returns a pointer to the array, or NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i = 0;
+
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	if (s == 0)
+		return (NULL);
+
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	return (s);
+}
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - a function that returns a pointer
+ * to a 2 dimensional array of integers
+ * @width: the input integer at number of columns
+ * @height: the input integer at number of rows
+ * Return: a pointer to a 2D array, NULL on failure
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **tD;
+	int hgt_i, wid_i;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	tD = malloc(sizeof(int *) * height);
+
+	if (tD == NULL)
+		return (NULL);
+
+	for (hgt_i = 0; hgt_i < height; hgt_i++)
+	{
+		tD[hgt_i] = malloc(sizeof(int) * width);
+
+		if (tD[hgt_i] == NULL)
+		{
+			for (; hgt_i >= 0; hgt_i--)
+				free(tD[hgt_i]);
+
+			free(tD);
+			return (NULL);
+		}
+	}
+	for (hgt_i = 0; hgt_i < height; hgt_i++)
+	{
+		for (wid_i = 0; wid_i < width; wid_i++)
+			tD[hgt_i][wid_i] = 0;
+	}
+	return (tD);
+}
