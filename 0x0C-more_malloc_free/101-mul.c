b4423985257c879acfefce21c1da8a8b98eeccf1
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-07-01 12:48:52 +0200

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,89 +3,83 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
-
 int check_error(char **argv, int argc);
 int l1, l2, i, j, tmp, chkk;
 int num1[2000], num2[2000], mul[4000] = {0};
-
 /**
  * main - multiplies two positive numbers
- * @argv: args
  * @argc: n arguments
+ * @argv: args
  * Return: int
  */
 
 int main(int argc, char **argv)
 {
-	if (argc != 3)
-	{
-		printf("error\n");
-		exit(98);
-	}
-	chkk = check_error(argv, argc);
-	if (chkk == 0)
-	{
-		l1 = strlen(argv[1]);
-		l2 = strlen(argv[2]);
-		for (i = l1 - 1, j = 0; i >= 0; i --, j++)
-		{
-			num1[j] = argv[1][i] - '0';
-		}
-		for (i = l2 - 1, j = 0; i >= 0; i --, j++)
-		{
-			num2[j] = arg[2][i] - '0';
-		}
-		for (i =0; i <l2; i++)
-		{
-			for (j = o; j < l1; j++)
-			{
-				mul[i=j] += num2[i] * num1[j];
-			}
-		}
-		for (i = 0; i < l1 + 12; i ++)
-		{
-			tmp = mul[i] / 10;
-			mul[i] = mul [i] % 10;
-			mul[i +1] = mul[i + 1] + tmp;
-		}
-		for (i = l1 =l2; i>= 0;i --)
-		{
-			if *mul[i]> 0
-				break;
-		}
-		for (; i >=0; i--)
-		{
-			print9 "%d" , mul[i]);
-		}
-	}
-
-	printf("\n");
-	return (0);
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
 }
+chkk = check_error(argv, argc);
+if (chkk == 0)
+{
+l1 = strlen(argv[1]);
+l2 = strlen(argv[2]);
+for (i = l1 - 1, j = 0; i >= 0; i--, j++)
+{
+num1[j] = argv[1][i] - '0';
+}
+for (i = l2 - 1, j = 0; i >= 0; i--, j++)
+{
+num2[j] = argv[2][i] - '0';
+}
+for (i = 0; i < l2; i++)
+{
+for (j = 0; j < l1; j++)
+{
+mul[i + j] += num2[i] * num1[j];
+}
+}
+for (i = 0; i < l1 + l2; i++)
+{
+tmp = mul[i] / 10;
+mul[i] = mul[i] % 10;
+mul[i + 1] = mul[i + 1] + tmp;
+}
+for (i = l1 + l2; i >= 0; i--)
+{
+if (mul[i] > 0)
+break;
+}
+for (; i >= 0; i--)
+{
+printf("%d", mul[i]);
+}
+}
+
+
+printf("\n");
+return (0);
+}
+
 
 /**
  * check_error - check error
- * argc: args
  * @argc: n arguments
+ * @argv: args
  * Return: int
  */
-int check_error(char **argv, int arfc)
+int check_error(char **argv, int argc)
 {
-	int i, j;
-	fot (i = 1; i< argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] != '\0'; j++)
-			{
-				if (argv[i][j] > 57 || argv[i][j] < 48)
-				{
-					printf("Error\n");
-					exit (98); 
-				}
-			}
-		}
-	}
+int i, j;
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] > 57 || argv[i][j] < 48)
+{  printf("Error\n");
+exit(98); }
+}
 }
 return (0);
 }
