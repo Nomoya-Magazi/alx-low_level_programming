f966762bcc8c342d65059cd95f675afd8be3a4a7
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-07-04 21:47:43 +0200

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-/**
+/*
  * Header File: 3-function_like_macro.h
  * Desc: header file that defines a function-like macro ABS(x)
  * that computes the absolute value of a number x
