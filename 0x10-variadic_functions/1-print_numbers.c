abfa5dd297548a124dd77b3bf90792a404f2f16c
Nomoya Magazi <nomoya.magazi@gmail.com>
2022-07-07 09:07:48 -0700

    variadic functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - a function that prints numbers, followed by a new line
+ * @separator: the string to be printed
+ * @n: the number of integers passed to the function
+ *
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int i;
+
+	va_start(nums, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(nums, int));
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(nums);
+}
+
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,30 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/**
+ * Desc: header file with all prototypes
+ *
+ */
+
+#include <stdarg.h>
+
+/**
+ * struct printer - a new struct type defining a printer.
+ * @symbol: a symbol representing a data type.
+ * @print: a function pointer to a function that prints
+ * a data type corresponding to symbol.
+ */
+
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+} printer_t;
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
+
+#endif
