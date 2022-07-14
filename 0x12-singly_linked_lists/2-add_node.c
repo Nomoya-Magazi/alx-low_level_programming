6a1c2b064da412e70805f132e8191a91da820905
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-07-14 20:16:46 +0200

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,8 +18,8 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
+
 	new_node->str = strdup(str);
-	
 	if (new_node->str == NULL)
 	{
 		free(new_node);
@@ -31,7 +31,7 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	new_node->len = len_str;
 	new_node->next = *head;
-	*head = new_node
-		
-		return (new_node);
+	*head = new_node;
+
+	return (new_node);
 }
