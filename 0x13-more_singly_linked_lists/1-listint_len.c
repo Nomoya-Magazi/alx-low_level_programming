14de36918bfd5784c99e4b2282b358bca29f8051
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-07-18 19:39:47 +0200

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,12 +7,12 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t elements = 0;
+size_t i = 0;
 
-	while (h)
-	{
-		h = h->next;
-		elements++;
-	}
-	return (num of elements);
+while (h)
+{
+h = h->next;
+i++;
+}
+return (i);
 }
