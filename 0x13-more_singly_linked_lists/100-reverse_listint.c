8f6098b7b1c0787b997374dc12cf32b50630e8d2
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-07-19 12:13:07 +0200

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -5,7 +5,7 @@
  * @head: Pointer to a pointer pointing to a struct
  *
  * Return: Returns a pointer to the first node of the reversed list
-**/
+ */
 
 listint_t *reverse_listint(listint_t **head)
 {
