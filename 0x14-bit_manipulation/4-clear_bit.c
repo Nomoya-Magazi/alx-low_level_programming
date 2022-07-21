ab9bd6885a9ecd11d04b42cb6c754c3c18b7b8f5
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-07-21 17:55:49 +0200

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,12 +9,8 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
-
+	if (index > sizeof(n) * 8)
 		return (-1);
-
-	m = (1 << index);
-	*n = *n & ~m;
-	
+	*n &= ~(1 << index);
 	return (1);
 }
