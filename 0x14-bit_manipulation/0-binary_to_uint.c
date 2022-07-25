f5fc3bc0e65f5b856dadcf9a03448cf8b9437ba7
Nomoya Magazi <nomoya.magazi@gmail.com>
2022-07-25 07:21:10 -0700

    updated file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
  * binary_to_uint - a function that converts a binary number to an unsigned int
  * @b: pointer to the string
  *
- * Return: the converted number, or 0 if 
+ * Return: the converted number, or 0 if
  * there is one or more chars in the string b that is not 0 or 1
  */
 
