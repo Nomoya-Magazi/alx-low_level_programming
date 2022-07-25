8218049dce979ac9327c1ca736e5e7e7d11d0db5
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-07-25 17:33:40 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,29 @@
 #include "main.h"
 /**
- * _strlen - finds the length of the string
- * @str: input
- *
- * Return: length of the string
- */
-size_t _strlen(char *str)
-{
-	size_t i;
-
-	for (i = 0; str[i]; i++)
-		return (i);
-}
-
-
-/**
- * create_file - a function that creates a file.
- * @filename: the filename to be created
- * @text_content: a NULL terminated string to write to the file
- * Return:1 on success, -1 on failure
+ * create_file - creates a file.
+ * @filename: a pointer to the name of the file to create.
+ * @text_content: a pointer to a string to write to the file.
+ * Return: If the function fails - -1, otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t len = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
+
 	if (text_content != NULL)
-		len = write(fd, text_content, _strlen(text_content));
-	close(fd);
-	if (len == -1)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
-	return (-1);
+
+	close(o);
+	return (1);
 }
