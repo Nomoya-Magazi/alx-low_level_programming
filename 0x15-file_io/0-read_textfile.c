9e85e20051366924385ce3bad539315b38a593d4
Nomoya Magazi <nomoya.magazi@gmail.com>
2022-07-25 07:52:38 -0700

    created files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * read_textfile - a function that reads a text file
+ * and prints it to the POSIX standard output.
+ * @filename: the pointer to the file
+ * @letters: the characters of the files
+ * Return:the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, l, i;
+	char *buffer;
+
+	buffer = malloc(sizeof(vhar) * lettters);
+
+	if (!buffer)
+	{
+		return (0);
+	}
+
+	if (!filename)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	{
+		if (fd == -1)
+		{
+			return (0);
+		}
+	}
+	l = read(fd, buffer, letters);
+	{
+		if (l == -1)
+		{
+			return (0);
+		}
+	}
+	i = write(STDOUT_FILENO, butter, l);
+	{
+		if (i == -1)
+		{
+			return (0);
+		}
+	}
+	close(fd);
+	free(buffer);
+	return (i);
+}
