de9eefce098bd0b852a16c1b3e3147deb73c1a35
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-09-05 23:32:54 +0200

    Update main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,10 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,5 +26,11 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
+extern div_t div(int __numer, int __denom);
 
 #endif
