cb2a922bcd52b8641c4662e27b09f54a07daf8ff
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-09-06 17:39:11 +0200

    Update 2-strlen.c

--- 0x18-dynamic_libraries/2-strlen.c
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -1,18 +1,15 @@
 #include "main.h"
-
 /**
- * _strlen - Return the length of a string
- * @str: string
- *
- * Return: Length
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: returns lenght;
  */
-
-int _strlen(char*s)
+int _strlen(char *s)
 {
-	int len = 0;
-
-	while(*s++)
-		len++;
-
-	return(len);
+	int count, inc;
+	inc = 0;
+	for (count = 0; s[count] != '\0'; count++)
+		inc++;
+	
+	return (inc);
 }
