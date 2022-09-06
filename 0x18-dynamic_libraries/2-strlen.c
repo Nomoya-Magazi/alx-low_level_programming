7b1242f1d8f0abbb439ea95e4fd744c7cd43fc34
Nomoya Magazi <nomoya.magazi@gmail.com>
2022-09-06 09:02:08 -0700

    betty files

--- 0x18-dynamic_libraries/2-strlen.c
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -4,24 +4,12 @@
  * @s: string
  * Return: returns lenght;
  */
-<<<<<<< HEAD
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s++)
-		len++;
-
-	return (len);
-=======
 int _strlen(char *s)
 {
 	int count, inc;
+
 	inc = 0;
 	for (count = 0; s[count] != '\0'; count++)
 		inc++;
-	
 	return (inc);
->>>>>>> cb2a922bcd52b8641c4662e27b09f54a07daf8ff
 }
