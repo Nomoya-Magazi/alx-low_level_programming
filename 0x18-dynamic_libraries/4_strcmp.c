6ce4337ba263e48afd30959170456c14c7b38563
Nomoya Magazi <nomoya.magazi@gmail.com>
2022-09-06 09:24:41 -0700

    betty file

--- 0x18-dynamic_libraries/4_strcmp.c
+++ 0x18-dynamic_libraries/4_strcmp.c
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-int _strcmp(char *s1, char *s2)
-{
-	return (0);
-}
-=======
-
->>>>>>> 3a89fd095a380fe995434762f4bec50465a50b00
-=======
 #include "main.h"
 /**
  * _strcmp - compares two strings
@@ -25,4 +15,3 @@ int _strcmp(char *s1, char *s2)
 	}
 	return (*s1 - *s2);
 }
->>>>>>> 800da17f651747bd4e380e828b3a5431c5a8dfb7
